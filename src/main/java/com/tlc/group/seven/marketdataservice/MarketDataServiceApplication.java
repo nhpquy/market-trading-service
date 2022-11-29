07d2b5f17613f6f0df3eb7b5a437ed822ea2980b
nhpquy <nhpquyit@gmail.com>
2022-11-29 13:02:54 +0000

    Created market data subscription

--- src/main/java/com/tlc/group/seven/marketdataservice/MarketDataServiceApplication.java
+++ src/main/java/com/tlc/group/seven/marketdataservice/MarketDataServiceApplication.java
@@ -2,12 +2,22 @@ package com.tlc.group.seven.marketdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MarketDataServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketDataServiceApplication.class, args);
 	}
+
+	@PostMapping("/api/latest/pd")
+	public void printData(@RequestBody OrderData data){	
+		System.out.println(data);
+	}
+
 
 }
