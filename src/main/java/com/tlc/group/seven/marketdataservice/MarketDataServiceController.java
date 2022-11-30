d908cf9ac31bf926ec89b36c9d4e8aed1d3631fa
nhpquy <nhpquyit@gmail.com>
2022-11-30 15:42:36 +0000

    Clean up code

--- src/main/java/com/tlc/group/seven/marketdataservice/MarketDataServiceController.java
+++ src/main/java/com/tlc/group/seven/marketdataservice/MarketDataServiceController.java
@@ -1,21 +1,11 @@
 package com.tlc.group.seven.marketdataservice;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import reactor.core.publisher.Mono;
 
 
 @RestController
@@ -27,6 +17,7 @@ public class MarketDataServiceController {
     @PostMapping("/api/latest/pd")
 	public void latestOrder(@RequestBody OrderData data){	
 		System.out.println(data);
+		
 	}
 
 	@GetMapping("/market-data")
--- src/main/java/com/tlc/group/seven/marketdataservice/OrderData.java
+++ src/main/java/com/tlc/group/seven/marketdataservice/OrderData.java
@@ -1,6 +1,5 @@
 package com.tlc.group.seven.marketdataservice;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
